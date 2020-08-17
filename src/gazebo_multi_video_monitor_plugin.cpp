b9184ff04feab1e2f1e6ac2d685152bfa325342c
nlamprian <nlamprian@gmail.com>
2020-08-17 23:51:04 +0200

    Add license notice

--- src/gazebo_multi_video_monitor_plugin.cpp
+++ src/gazebo_multi_video_monitor_plugin.cpp
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2020  Nikolaos Lamprianidis
+ *
+ * Gazebo Video Monitor Plugins is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as  published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Gazebo Video Monitor Plugins is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <ctime>
 
 #include <gazebo_video_monitor_plugins/gazebo_multi_video_monitor_plugin.h>
